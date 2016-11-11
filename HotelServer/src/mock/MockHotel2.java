9447c6adbd56ee89d586b965031f5ee804aba231
yuanyanyan1452 <857364134@qq.com>
2016-11-11 10:26:07 +0800

    Create MockHotel2.java

---
+++ HotelServer/src/mock/MockHotel2.java
@@ -0,0 +1,36 @@
+package mock;
+
+import objects.Hotel;
+
+public class MockHotel2 extends Hotel{
+	private String address;
+	private String business_address;
+	private String name;
+	private String introduction;
+	private String service;
+	private String room_state;
+	private String room_type;
+	private int room_number;
+	private int room_price;
+	private String order;
+	private String evaluation;
+	
+	public MockHotel2(String a,String ba,String na,String in,String s,String rs,String rt,int rn,int rp,String o,String e){
+		this.address=a;
+		this.business_address=ba;
+		this.name=na;
+		this.introduction=in;
+		this.service=s;
+		this.room_state=rt;
+		this.room_type=rt;
+		this.room_number=rn;
+		this.room_price=rp;
+		this.order=o;
+		this.evaluation=e;
+	}
+	
+	public MockHotel2 getInfo(){
+		return new MockHotel2(address,business_address,name,introduction,service,room_state,room_type,
+				room_number,room_price,order,evaluation);
+    }
+}

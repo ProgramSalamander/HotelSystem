356787f57898cb5fab08fc4c3ddff13b56faa16b
jiaweiXu <151250171@smail.nju.edu.cn>
2016-11-13 10:27:56 +0800

    Update VIPInfo.java

--- HotelServer/src/objects/VIPInfo.java
+++ HotelServer/src/objects/VIPInfo.java
@@ -1,10 +1,22 @@
-package objects;
-
+ package objects;
 
 public class VIPInfo {
-		public enum VIPType{NORMAL,Enterprise};
-		VIPType type;
-		int VIPid;
-		String birthday;
-		String enterpriseName;
+	public enum VIPType {
+		NORMAL, Enterprise
+	};
+
+	VIPType type;
+	int VIPid;
+	String birthday;
+	String enterpriseName;
+
+	public VIPInfo(VIPType type, int vIPid, String info) {
+		this.type = type;
+		VIPid = vIPid;
+		if(type == VIPType.NORMAL)
+			birthday = info;
+		else
+			enterpriseName = info;
+	}
 }
+

14fa72fa46c33ff3b0eb117c5ebb494b89c8fead
xyc <464243149@qq.com>
2016-11-13 09:37:02 +0800

    更新clienttest
    将测试对象改为clientblserviceimpl

--- HotelServer/src/test/Clienttest.java
+++ HotelServer/src/test/Clienttest.java
@@ -11,16 +11,16 @@ import objects.RoomType;
 import objects.VIPInfo;
 import objects.VIPInfo.VIPType;
 import service.blservice.ClientBLService;
+import service.blservice.Impl.ClientBLServiceImpl;
 import vo.ClientVO;
 import vo.HotelVO;
 import mock.*;
 
 public class Clienttest {
-	ClientBLService clientcontroller = new ClientController();
-
 	// 测试提供给界面的接口
 	@Test
 	public void client_checkInfoTest() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		ArrayList<String> credit_record = new ArrayList<String>();
 		VIPInfo info = new VIPInfo(VIPType.NORMAL, 1, "2000/01/01");
 		ClientVO vo = new ClientVO(1, "张三", "11111111111", 0, credit_record, info);
@@ -29,6 +29,7 @@ public class Clienttest {
 
 	@Test
 	public void client_updateInfoTest() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		ArrayList<String> credit_record = new ArrayList<String>();
 		VIPInfo info = new VIPInfo(VIPType.NORMAL, 1, "2000/01/01");
 		ClientVO vo = new ClientVO(1, "张三", "11111111111", 0, credit_record, info);
@@ -37,6 +38,7 @@ public class Clienttest {
 
 	@Test
 	public void client_getpreviousHotelListTest() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		ArrayList<HotelVO> HotelList_Client = new ArrayList<HotelVO>();
 		HotelVO hotel = new HotelVO("南京", "新街口", "如家", "如家酒店", "酒店所有服务", "可用", "双人房", 1, 100, "", "good");
 		HotelList_Client.add(hotel);
@@ -45,6 +47,7 @@ public class Clienttest {
 
 	@Test
 	public void client_setLocationTest() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		MockHotel30 hotel30 = new MockHotel30("南京");
 		ArrayList<HotelVO> HotelList_Client = new ArrayList<HotelVO>();
 		HotelVO hotel = new HotelVO("南京", "新街口", "如家", "如家酒店", "酒店所有服务", "可用", "双人房", 1, 100, "", "good");
@@ -54,6 +57,7 @@ public class Clienttest {
 
 	@Test
 	public void client_searchHotelTest1() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		MockHotel31 hotel31 = new MockHotel31("如家");
 		ArrayList<HotelVO> HotelList_Client = new ArrayList<HotelVO>();
 		HotelVO hotel = new HotelVO("南京", "新街口", "如家", "如家酒店", "酒店所有服务", "可用", "双人房", 1, 100, "", "good");
@@ -63,6 +67,7 @@ public class Clienttest {
 
 	@Test
 	public void client_searchHotelTest2() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		MockHotel32 hotel32 = new MockHotel32(RoomType.DOUBLEROOM);
 		ArrayList<HotelVO> HotelList_Client = new ArrayList<HotelVO>();
 		HotelVO hotel = new HotelVO("南京", "新街口", "如家", "如家酒店", "酒店所有服务", "可用", "双人房", 1, 100, "", "good");
@@ -72,6 +77,7 @@ public class Clienttest {
 
 	@Test
 	public void client_searchHotelTest3() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		MockHotel33 hotel33 = new MockHotel33(100, 200);
 		ArrayList<HotelVO> HotelList_Client = new ArrayList<HotelVO>();
 		HotelVO hotel = new HotelVO("南京", "新街口", "如家", "如家酒店", "酒店所有服务", "可用", "双人房", 1, 100, "", "good");
@@ -82,6 +88,7 @@ public class Clienttest {
 
 	@Test
 	public void client_searchHotelTest4() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		MockHotel34 hotel34 = new MockHotel34("2001/01/01", "2001/01/03");
 		ArrayList<HotelVO> HotelList_Client = new ArrayList<HotelVO>();
 		HotelVO hotel = new HotelVO("南京", "新街口", "如家", "如家酒店", "酒店所有服务", "可用", "双人房", 1, 100, "", "good");
@@ -92,6 +99,7 @@ public class Clienttest {
 
 	@Test
 	public void client_searchHotelTest5() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		MockHotel35 hotel35 = new MockHotel35(1);
 		ArrayList<HotelVO> HotelList_Client = new ArrayList<HotelVO>();
 		HotelVO hotel = new HotelVO("南京", "新街口", "如家", "如家酒店", "酒店所有服务", "可用", "双人房", 1, 100, "", "good");
@@ -101,6 +109,7 @@ public class Clienttest {
 
 	@Test
 	public void client_searchHotelTest6() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		MockHotel36 hotel36 = new MockHotel36(4.0, 5.0);
 		ArrayList<HotelVO> HotelList_Client = new ArrayList<HotelVO>();
 		HotelVO hotel = new HotelVO("南京", "新街口", "如家", "如家酒店", "酒店所有服务", "可用", "双人房", 1, 100, "", "good");
@@ -111,18 +120,21 @@ public class Clienttest {
 
 	@Test
 	public void client_checkHotelInfoTest() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		HotelVO hotel = new HotelVO("南京", "新街口", "如家", "如家酒店", "酒店所有服务", "可用", "双人房", 1, 100, "", "good");
 		Assert.assertEquals(hotel, clientcontroller.client_checkHotelInfo(1));
 	}
 
 	@Test
 	public void client_checkCreditTest() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		MockClient30 client30 = new MockClient30(1);
 		Assert.assertEquals(0, clientcontroller.client_checkCredit(client30.getId()));
 	}
 
 	@Test
 	public void client_checkCreditListTest() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		MockClient30 client30 = new MockClient30(1);
 		ArrayList<String> strings = new ArrayList<>();
 		Assert.assertEquals(strings, clientcontroller.client_checkCreditList(client30.getId()));
@@ -130,6 +142,7 @@ public class Clienttest {
 
 	@Test
 	public void client_evaluateHotelTest() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		MockClient30 client30 = new MockClient30(1);
 		Evaluation evaluation = new Evaluation(4.0, "good");
 		Assert.assertEquals(ResultMessage.Success, clientcontroller.client_evaluateHotel(evaluation, client30.getId()));
@@ -137,6 +150,7 @@ public class Clienttest {
 
 	@Test
 	public void client_enrollVIPTest() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		MockClient30 client30 = new MockClient30(1);
 		VIPInfo info = new VIPInfo(VIPType.NORMAL, 1, "2001/01/01");
 		Assert.assertEquals(ResultMessage.Success, clientcontroller.client_enrollVIP(info, client30.getId()));
@@ -145,17 +159,20 @@ public class Clienttest {
 	// 测试提供给同层的接口
 	@Test
 	public void updateClientCreditTest() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		Assert.assertEquals(ResultMessage.Success, clientcontroller.updateClientCredit(1, 100, 0));
 	}
 
 	@Test
 	public void checkClientInfoTest() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		MockClient1 client1 = new MockClient1(1);
 		Assert.assertEquals(client1, clientcontroller.checkClientInfo(1));
 	}
 
 	@Test
 	public void updateClientInfoTest() {
+		ClientBLService clientcontroller = new ClientBLServiceImpl();
 		MockClient1 client2 = new MockClient1(2);
 		Assert.assertEquals(ResultMessage.Success, clientcontroller.updateClientInfo(client2));
 	}

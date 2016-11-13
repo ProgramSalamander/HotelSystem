fb8f0c67b33d9105a1b8f01f63ac4ea8645d63c3
jiaweiXu <151250171@smail.nju.edu.cn>
2016-11-13 10:24:24 +0800

    Update ClientVO.java

--- HotelServer/src/vo/ClientVO.java
+++ HotelServer/src/vo/ClientVO.java
@@ -1,54 +1,74 @@
 package vo;
 
+import java.util.ArrayList;
+
+import objects.VIPInfo;
 
 public class ClientVO {
 	int clientid;
-	String name;
+	String client_name;
 	String contact;
 	int credit;
-	String credit_record;
-	int memberid;
-	String member_type;
-	String birthday;
-	String company;
-	
-	public ClientVO(int id,String n,String c,int c1,String cr,int m,String mt,String b,String co){
-		clientid = id;
-		name=n;
-		contact=c;
-		credit=c1;
-		credit_record=cr;
-		memberid=m;
-		member_type=mt;
-		birthday=b;
-		company=co;
+	ArrayList<String> credit_record;
+	VIPInfo info;
+
+	public ClientVO(int clientid, String client_name, String contact, int credit, ArrayList<String> credit_record,
+			VIPInfo info) {
+		super();
+		this.clientid = clientid;
+		this.client_name = client_name;
+		this.contact = contact;
+		this.credit = credit;
+		this.credit_record = credit_record;
+		this.info = info;
 	}
-	
-	public int getid(){
+
+	public int getClientid() {
 		return clientid;
 	}
-	public String getname(){
-		return name;
+
+	public void setClientid(int clientid) {
+		this.clientid = clientid;
 	}
-	public String getcontact(){
+
+	public String getClient_name() {
+		return client_name;
+	}
+
+	public void setClient_name(String client_name) {
+		this.client_name = client_name;
+	}
+
+	public String getContact() {
 		return contact;
 	}
-	public int getcredit(){
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public int getCredit() {
 		return credit;
 	}
-	public String getcredit_record(){
+
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+
+	public ArrayList<String> getCredit_record() {
 		return credit_record;
 	}
-	public int getmemberid(){
-		return memberid;
+
+	public void setCredit_record(ArrayList<String> credit_record) {
+		this.credit_record = credit_record;
 	}
-	public String getmember_type(){
-		return member_type;
+
+	public VIPInfo getInfo() {
+		return info;
 	}
-	public String getbirthday(){
-		return birthday;
+
+	public void setInfo(VIPInfo info) {
+		this.info = info;
 	}
-	public String getcompany(){
-		return company;
-	}	
+
 }
